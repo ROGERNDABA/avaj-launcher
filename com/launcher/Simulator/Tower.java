00500a55d77985623a5caf2886ea2c5fbf65f4d0
rogerndaba <rogerndaba@gmail.com>
2019-08-09 12:00:23 +0200

    started on tower

---
+++ com/launcher/Simulator/Tower.java
@@ -0,0 +1,25 @@
+package com.launcher.Simulator;
+
+import com.launcher.Simulator.Flyable;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Tower
+ */
+public class Tower {
+
+	private List<Flyable> observer = new ArrayList<Flyable>();
+
+	public void register(Flyable flyable) {
+		this.observer.add(flyable);
+	}
+
+	public void unregister(Flyable flyable) {
+		this.observer.add(flyable);
+	}
+
+	protected void conditionsChanged() {
+
+	}
+}
--- com/launcher/Simulator/WeatherTower.java
+++ com/launcher/Simulator/WeatherTower.java
@@ -1,8 +1,13 @@
 package com.launcher.Simulator;
 
+import com.launcher.Simulator.Coordinates;
+
 /**
  * WeatherTower
  */
 public class WeatherTower {
+	public String getWeather(Coordinates coordinates) {
+		return "Roger";
+	}
 
 }
