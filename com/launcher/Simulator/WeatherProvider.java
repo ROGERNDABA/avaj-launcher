e507763d0fbaa167cc444892a0dab658e33f3f1f
rogerndaba <rogerndaba@gmail.com>
2019-08-10 11:38:41 +0200

    probably done WeatherProvider, not sure it works

--- com/launcher/Simulator/WeatherProvider.java
+++ com/launcher/Simulator/WeatherProvider.java
@@ -1,8 +1,25 @@
 package com.launcher.Simulator;
 
+import java.util.Random;
+
+import com.launcher.Simulator.Coordinates;
+
 /**
  * WeatherProvider
  */
 public class WeatherProvider {
+	private WeatherProvider weatherProvider = new WeatherProvider();
+	private String[] weather = { "SUN", "FOG", "SNOW", "RAIN" };
 
+	private WeatherProvider() {
+	}
+
+	public WeatherProvider getWeather() {
+		return this.weatherProvider;
+	}
+
+	public String getCurrentWeather(Coordinates coordinates) {
+		int rand = new Random().nextInt(4);
+		return weather[rand];
+	}
 }
